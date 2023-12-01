749eea9184b43a203ec7405a57497724ed2c1db1
Drew Schillinger <drew@doctorew.com>
2023-12-01 10:31:44 -0500

    styling but spinner not displaying

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,17 +3,20 @@
 
 // Then, in your page file (e.g., src/pages/index.tsx):
 import { useState, FormEvent } from 'react';
-import type { Message } from '../types'; // Adjust the path as necessary
+import type { Message } from '../types'; // Adjust the pa
+import { Spinner } from '@nextui-org/react';// th as necessary
 
 export default function Home() {
 	const [query, setQuery] = useState<string>('');
 	const [conversation, setConversation] = useState<Message[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false); // New state for loading
 
 	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		const userQuery = query;
 		setConversation((prev) => [...prev, { type: 'user', text: userQuery }]);
 		setQuery('');
+		setIsLoading(true); // Set loading to true
 
 		try {
 			const res = await fetch('/api/alorica', {
@@ -28,33 +31,43 @@ export default function Home() {
 		} catch (error) {
 			console.error('Error:', error);
 			setConversation((prev) => [...prev, { type: 'user', text: userQuery }, { type: 'bot', text: 'Error communicating with the server.' }]);
+		} finally {
+			setIsLoading(false); // Set loading to false
 		}
 	};
 
 	return (
-		<div className="max-w-lg mx-auto my-10 p-4">
-			<div className="mb-4">
-				{conversation.map((message, index) => (
-					<div
-						key={index}
-						className={`p-4 rounded-lg text-white my-2 ${message.type === 'user' ? 'bg-blue-500 ml-auto' : 'bg-green-500 mr-auto'}`}
-					>
-						{message.text}
-					</div>
-				))}
+		<div className="min-h-screen bg-aloricaLightGray">
+			<header className="bg-white py-4">
+				<div className="max-w-lg mx-auto flex justify-center">
+					<img src="https://www.alorica.com/images/default-source/alorica/website2-0__header-logo.svg?sfvrsn=54b0b15c_0" alt="Alorica Logo" className="h-8" />
+				</div>
+			</header>
+
+			<div className="max-w-lg mx-auto my-10 p-4">
+				<div className="mb-4">
+					{/* Message bubbles */}
+					{conversation.map((message, index) => (
+						<div key={index} className={`p-4 rounded-lg text-white my-2 ${message.type === 'user' ? 'bg-aloricaBlue ml-auto' : 'bg-aloricaGray mr-auto'}`}>
+							{message.text}
+						</div>
+					))}
+				</div>
+
+				{/* Chat input form */}
+				<form onSubmit={handleSubmit} className="flex">
+					<input
+						type="text"
+						value={query}
+						onChange={(e) => setQuery(e.target.value)}
+						placeholder="Ask about Alorica"
+						className="flex-1 p-2 border border-gray-300 rounded-l-lg focus:outline-none"
+					/>
+					<button type="submit" className="bg-aloricaBlue text-white p-2 rounded-r-lg">
+						Send
+					</button>
+				</form>
 			</div>
-			<form onSubmit={handleSubmit} className="flex">
-				<input
-					type="text"
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-					placeholder="Ask about Alorica"
-					className="flex-1 p-2 border border-gray-300 rounded-l-lg focus:outline-none"
-				/>
-				<button type="submit" className="bg-blue-500 text-white p-2 rounded-r-lg">
-					Send
-				</button>
-			</form>
 		</div>
 	);
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import { Spinner } from "next/components";
 
 const config: Config = {
   content: [
@@ -8,13 +9,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        aloricaBlue: '#00a9e0', // Custom blue color used in Alorica branding
+        aloricaGray: '#464646', // Custom gray color
+        aloricaLightGray: '#f4f4f4', // Custom light gray color
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      // Add any additional theme extensions here
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
